0db2d64d313106f7e250dfb822848d20462e219c
PhattaraPCRU Nitro5 <st641102064111@office365.pcru.ac.th>
2022-09-14 11:35:18 +0700

    first commit

---
+++ src/main/java/phattara/tempconverter/TempConverter.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author koonp@641102064111
+ * Create on 9:51:15 AM Sep 14, 2022
+ * This file is part of "phattara.tempconverter" Package (First).
+ */
+package phattara.tempconverter;
+
+/**
+ *
+ * @author koonp
+ */
+public class TempConverter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
